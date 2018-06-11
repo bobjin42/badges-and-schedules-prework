5c5d39a8c5fb8b8d8077eb9e850ced53f20ed88c
Yicheng Jin <bobjin42@gmail.com>
2018-06-11 17:16:38 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,7 +17,7 @@ end
 
 def assign_rooms(array)
   new_array = []
-  each_with_index do|item, index|
+  array.each_with_index do|item, index|
   phrase = "Hello, #{item}! You'll be assigned to room #{index + 1}"
   new_array.push(phrase)
   end
